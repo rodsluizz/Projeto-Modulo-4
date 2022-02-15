c1f53d0d81f781e5de25b90532bdf1c77b5d60d7
rodsluizz <rodrigoluizcferreira@gmail.com>
2022-02-14 23:56:48 -0300

    front-end projeto modulo 4

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -1,7 +1,24 @@
-import React from "react"
+import React from "react";
+import "../contato/Contato.css";
+import imgBackground from "../../Assets/imgs/backgroundimage.png";
 
 export default function Contato(){
     return(
-        <h1>CONTATO</h1>
+    <>
+        <section>
+            <img src={imgBackground} alt="" />
+            <form action="" autoComplete="on" className="formulario__">
+                <label htmlFor="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" />
+                <label htmlFor="telefone">Telefone:</label>
+                <input type="tel" id="telefone" name="telefone" />
+                <label for="caixatexto">Deixe uma mensagem:</label>
+                <textarea name="caixatexto" id="caixaMensagem" cols="40" rows="10"></textarea>
+                <button class="botao">Enviar</button>
+            </form>
+        </section>
+    </>
     )
 }
--- src/components/internacionais/Internacionais.jsx
+++ src/components/internacionais/Internacionais.jsx
@@ -1,7 +1,49 @@
-import React from "react"
+import React from "react";
+import "../internacionais/Internacionais.css";
+import imgRoma from "../../Assets/imgs/europa/roma.jpg";
+import imgBerlim from "../../Assets/imgs/europa/berlim.jpg";
+import imgParis from "../../Assets/imgs/europa/paris.jpg";
+import imgLondres from "../../Assets/imgs/europa/londres.jpg";
 
 export default function Internacionais(){
     return(
-        <h1>EM CONSTRUÇÃO</h1>
+        <>
+            <h1>Roteiros Internacionais</h1>
+            <section className="viagensSection">
+                <div className="spaceEuropa">
+                    <img src={imgRoma} alt="" />
+                    <div className="descricaoViagem">
+                        <h4>Roma</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, modi, ipsam quos perferendis recusandae autem reiciendis nihil eveniet magni eos obcaecati deserunt odio exercitationem commodi quas, ut ipsum. Mollitia, laborum.</p>
+                        <button>Comprar <i class="fas fa-shopping-cart"></i></button>
+                    </div>
+                </div>
+                <div className="spaceEuropa">
+                    <img src={imgParis} alt="" />
+                    <div className="descricaoViagem">
+                        <h4>Paris</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, modi, ipsam quos perferendis recusandae autem reiciendis nihil eveniet magni eos obcaecati deserunt odio exercitationem commodi quas, ut ipsum. Mollitia, laborum.</p>
+                        <button>Comprar <i class="fas fa-shopping-cart"></i></button>
+                    </div>
+                </div>
+                <div className="spaceEuropa">
+                    <img src={imgBerlim} alt="" />
+                    <div className="descricaoViagem">
+                        <h4>Berlim</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, modi, ipsam quos perferendis recusandae autem reiciendis nihil eveniet magni eos obcaecati deserunt odio exercitationem commodi quas, ut ipsum. Mollitia, laborum.</p>
+                        <button>Comprar <i class="fas fa-shopping-cart"></i></button>
+                    </div>
+                </div>
+                <div className="spaceEuropa">
+                    <img src={imgLondres} alt="" />
+                    <div className="descricaoViagem">
+                        <h4>Londres</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, modi, ipsam quos perferendis recusandae autem reiciendis nihil eveniet magni eos obcaecati deserunt odio exercitationem commodi quas, ut ipsum. Mollitia, laborum.</p>
+                        <button>Comprar <i class="fas fa-shopping-cart"></i></button>
+                    </div>
+                </div>
+            </section>
+        </>
+        
     )
 }
