15321aa8305978d192e2ec21d1701a47bb46b2fb
rodsluizz <rodrigoluizcferreira@gmail.com>
2022-03-18 23:35:43 -0300

    Atualização 18/03 pt4

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import LoginButton from "../autenticacaoComponents/LoginButton";
 import LogoutButton from "../autenticacaoComponents/LogoutButton";
-import Profile from "../autenticacaoComponents/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Navbar() {
   const { isLoading } = useAuth0();
-
-  if(isLoading) return <div>Loading...</div>
 
   return (
     <>
